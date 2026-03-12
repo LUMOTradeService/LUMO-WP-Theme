8397b9f7332cc6ea24defb48ff8812dc6d4cfb79
Lukáš Moravec <39536753+LUMOL0ki@users.noreply.github.com>
2026-03-12 15:03:22 +0100

    Update service-container.php

--- patterns/service-container.php
+++ patterns/service-container.php
@@ -5,10 +5,11 @@
  * Categories: Services
  */
 ?>
+
 <!-- wp:group {"metadata":{"categories":["Services"],"patternName":"lumopos/service-container","name":"Service container"},"className":"is-style-group-secondary","style":{"spacing":{"blockGap":"var:preset|spacing|normal"},"dimensions":{"minHeight":"330px"},"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
-<div class="wp-block-group is-style-group-secondary" style="min-height:330px"><!-- wp:lumopos/icons {"iconName":"person"} -->
-<div class="wp-block-lumopos-icons"><svg xmlns="http://www.w3.org/2000/svg" width="48px" height="48px" fill="#FFF" viewBox="0 -960 960 960"><path d="M381.04-533.35Q340-574.38 340-632.31q0-57.92 41.04-98.96T480-772.31t98.96 41.04T620-632.31q0 57.93-41.04 98.96-41.04 41.04-98.96 41.04t-98.96-41.04M180-248.46v-28.16q0-29.38 15.96-54.42t42.66-38.5q59.3-29.07 119.65-43.61T480-427.69t121.73 14.54 119.65 43.61q26.7 13.46 42.66 38.5T780-276.62v28.16q0 25.3-17.73 43.04-17.73 17.73-43.04 17.73H240.77q-25.31 0-43.04-17.73Q180-223.16 180-248.46m60 .77h480v-28.93q0-12.15-7.04-22.5-7.04-10.34-19.11-16.88-51.7-25.46-105.35-38.58-53.65-13.11-108.5-13.11t-108.5 13.11Q317.85-341.46 266.15-316q-12.07 6.54-19.11 16.88-7.04 10.35-7.04 22.5zm296.5-328.12q23.5-23.5 23.5-56.5t-23.5-56.5-56.5-23.5-56.5 23.5-23.5 56.5 23.5 56.5 56.5 23.5 56.5-23.5M480-247.69"></path></svg></div>
-<!-- /wp:lumopos/icons -->
+<div class="wp-block-group is-style-group-secondary" style="min-height:330px"><!-- wp:lumopos/icon -->
+<div class="wp-block-lumopos-icon"><svg xmlns="http://www.w3.org/2000/svg" width="48px" height="48px" fill="#FFF" viewBox="0 -960 960 960"><path d="m379.15-323.15 363-363.39q6.7-6.31 16.12-6.5t16.5 6.69q7.08 6.89 7.08 16.31t-7.08 16.12L399.38-278.54q-8.61 8.62-20.23 8.62-11.61 0-20.23-8.62l-174.3-174.31q-6.7-6.69-6.58-16.11.11-9.42 7.19-16.31 7.08-6.88 16.31-6.88t16.31 6.69z"></path></svg></div>
+<!-- /wp:lumopos/icon -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":6} -->

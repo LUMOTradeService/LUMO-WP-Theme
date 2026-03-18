cbcf356f81a94afbe8cd0491d0ec8499ae1847e7
Lukáš Moravec <39536753+LUMOL0ki@users.noreply.github.com>
2026-03-18 11:32:08 +0100

    Updated theme UI

--- patterns/footer.php
+++ patterns/footer.php
@@ -1,52 +1,48 @@
 <?php
 /**
  * Title: footer
- * Slug: lumopos/footer
+ * Slug: lumo-wp-theme/footer
  * Inserter: no
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|extra-large","margin":{"bottom":"var:preset|spacing|extra-large"},"padding":{"right":"var:preset|spacing|large","left":"var:preset|spacing|large"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="margin-bottom:var(--wp--preset--spacing--extra-large);padding-right:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)"><!-- wp:group {"align":"full","className":"is-style-default","style":{"spacing":{"padding":{"right":"var:preset|spacing|large","left":"var:preset|spacing|large"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-default" style="padding-right:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)"><!-- wp:group {"metadata":{"categories":["footer"],"patternName":"core/left-aligned-footer","name":"Left-aligned footer"},"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|large","padding":{"right":"var:preset|spacing|large","left":"var:preset|spacing|large"}}},"layout":{"type":"grid","columnCount":null,"minimumColumnWidth":"320px"}} -->
-<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-right:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)"><!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|large"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"top"}} -->
-<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small)"><!-- wp:site-logo {"width":230,"style":{"layout":{"selfStretch":"fixed","flexSize":"30px"}}} /--></div>
-<!-- /wp:group -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|large","left":"var:preset|spacing|large"},"blockGap":"var:preset|spacing|extra-large"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)"><!-- wp:group {"metadata":{"categories":["footer"],"patternName":"core/left-aligned-footer","name":"Left-aligned footer"},"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"grid","minimumColumnWidth":null,"columnCount":2}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null,"columnSpan":1,"rowSpan":1}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"top","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:site-logo /-->
 
-<!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
-<p class="has-text-align-left"><?php esc_html_e('Každou zakázku řešíme individuálně, s důrazem na bezpečnost, kvalitu provedení a dlouhodobou spolehlivost.', 'lumopos');?></p>
+<!-- wp:paragraph {"align":"left"} -->
+<p class="has-text-align-left"><?php esc_html_e('Short company description about what this e-shop selling.', 'lumo-wp-theme');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|large"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group"><!-- wp:heading {"textAlign":"left","level":4} -->
-<h4 class="wp-block-heading has-text-align-left"><?php esc_html_e('Rychlé odkazy', 'lumopos');?></h4>
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"left","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"left","level":4,"fontSize":"title-large"} -->
+<h4 class="wp-block-heading has-text-align-left has-title-large-font-size"><?php esc_html_e('Quick links', 'lumo-wp-theme');?></h4>
 <!-- /wp:heading -->
 
-<!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"blockGap":"8px"}},"fontSize":"small","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} /--></div>
+<!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"fontSize":"small","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} /--></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|large"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group"><!-- wp:heading {"textAlign":"left","level":4} -->
-<h4 class="wp-block-heading has-text-align-left"><?php esc_html_e('Péče o zákazníka', 'lumopos');?></h4>
+<!-- wp:group {"style":{"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"left","level":4,"fontSize":"title-large"} -->
+<h4 class="wp-block-heading has-text-align-left has-title-large-font-size"><?php esc_html_e('Customer care', 'lumo-wp-theme');?></h4>
 <!-- /wp:heading -->
 
-<!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"blockGap":"8px"}},"fontSize":"small","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} /--></div>
-<!-- /wp:group -->
-
-<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|large"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group"></div>
+<!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"fontSize":"small","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"nowrap"}} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:separator {"className":"is-style-default"} -->
-<hr class="wp-block-separator has-alpha-channel-opacity is-style-default"/>
+<!-- wp:separator -->
+<hr class="wp-block-separator has-alpha-channel-opacity"/>
 <!-- /wp:separator -->
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|large","left":"var:preset|spacing|large"},"blockGap":"var:preset|spacing|extra-large"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)"><!-- wp:paragraph -->
-<p><?php esc_html_e('© 2026 LUMO trade service s.r.o. Všechna práva vyhrazena.', 'lumopos');?></p>
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"overlayMenu":"never"} /-->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('© 2026 LUMO trade service s.r.o. Všechna práva vyhrazena.', 'lumo-wp-theme');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
--- patterns/header.php
+++ patterns/header.php
@@ -9,11 +9,9 @@
 <div class="wp-block-group alignwide"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large)"><!-- wp:navigation {"overlayMenu":"always","icon":"menu","overlayBackgroundColor":"surface-container","overlayTextColor":"on-surface","layout":{"type":"flex","justifyContent":"left","flexWrap":"wrap"}} /-->
 
-<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:site-logo {"width":250} /-->
+<!-- wp:site-logo {"width":200} /-->
 
-<!-- wp:navigation {"overlayMenu":"never","icon":"menu","overlayBackgroundColor":"surface-container","overlayTextColor":"on-surface","layout":{"type":"flex","justifyContent":"left","flexWrap":"wrap"}} /--></div>
-<!-- /wp:group -->
+<!-- wp:navigation {"overlayMenu":"never","icon":"menu","overlayBackgroundColor":"surface-container","overlayTextColor":"on-surface","className":"absolute-center","layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap"}} /-->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->

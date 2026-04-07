a02b43d24b595c67f5653d714fcf6fddc00b670b
Lukáš Moravec <39536753+LUMOL0ki@users.noreply.github.com>
2026-04-07 15:21:46 +0200

    Add header with call to action

---
+++ patterns/header-with-call-to-action.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: header-with-call-to-action
+ * Slug: lumo-wp-theme/header-with-navigation-copy
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"align":"wide","className":"is-style-group-filled","style":{"spacing":{"padding":{"right":"var:preset|spacing|large-increased","left":"var:preset|spacing|large-increased"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide is-style-group-filled" style="padding-right:var(--wp--preset--spacing--large-increased);padding-left:var(--wp--preset--spacing--large-increased)"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large)"><!-- wp:navigation {"overlayMenu":"always","icon":"menu","overlayBackgroundColor":"surface-container","overlayTextColor":"on-surface","layout":{"type":"flex","justifyContent":"left","flexWrap":"wrap"}} /-->
+
+<!-- wp:site-logo {"width":200} /-->
+
+<!-- wp:navigation {"overlayMenu":"never","icon":"menu","overlayBackgroundColor":"surface-container","overlayTextColor":"on-surface","className":"absolute-center","layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap"}} /-->
+
+<!-- wp:buttons {"style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e('Dashboard', 'lumo-wp-theme');?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
